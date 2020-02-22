503ad1bb8eb4c7e2089a35c235a92a1a13e06a31
Dasmer Singh <dasmersingh@gmail.com>
2020-02-22 14:09:40 -0500

    First project commit

---
+++ Whichflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whichflix
+//
+//  Created by Dasmer Singh on 2/22/20.
+//  Copyright © 2020 Dastronics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
